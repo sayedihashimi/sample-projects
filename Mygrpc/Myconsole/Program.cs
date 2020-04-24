ac08229b99aacc632262bdb188cec63745197642
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-04-24 12:56:19 -0400

    now working

--- Mygrpc/Myconsole/Program.cs
+++ Mygrpc/Myconsole/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Mygrpc;
 
 namespace Myconsole {
     class Program {
-        static void Main(string[] args) {
+        static async Task Main(string[] args) {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
             Console.WriteLine("Hello World!");
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
