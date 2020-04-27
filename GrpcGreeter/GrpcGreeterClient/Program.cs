8605a25b3a2a64b2f798f4a0af637f410dd8efcc
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-04-27 12:11:37 -0400

    adding grpc sample

---
+++ GrpcGreeter/GrpcGreeterClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GrpcGreeter;
+using Grpc.Net.Client;
+
+namespace GrpcGreeterClient {
+    class Program {
+        static async System.Threading.Tasks.Task Main(string[] args) {
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            Console.WriteLine("Hello World!");
+
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
