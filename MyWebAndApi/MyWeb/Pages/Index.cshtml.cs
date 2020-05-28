cee0792eb7ffd6c7e10ccc8e3322d212795ed40d
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2020-05-28 10:28:55 -0400

    after testing api in web project, works

--- MyWebAndApi/MyWeb/Pages/Index.cshtml.cs
+++ MyWebAndApi/MyWeb/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -14,8 +15,12 @@ namespace MyWeb.Pages {
             _logger = logger;
         }
 
-        public void OnGet() {
-
+        public async Task OnGetAsync() {
+            string baseurl = "https://localhost:5001";
+            var httpclient = new HttpClient();
+            swaggerClient client = new swaggerClient(baseurl, httpclient);
+            var weather = await client.WeatherForecastAsync();
+            Console.WriteLine(weather.ElementAt(0).Summary);
         }
     }
 }
