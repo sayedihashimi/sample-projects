ff01c3ac6649d520286835b1cbfbc2abe047d126
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2025-08-07 15:33:45 -0400

    changes up to the copilot step applied here

--- PhotoGallery/PhotoGallery.AppHost/AppHost.cs
+++ PhotoGallery/PhotoGallery.AppHost/AppHost.cs
@@ -1,3 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var photos = builder.AddAzureStorage("storage")
+                .RunAsEmulator()
+                .AddBlobs("blobs")
+                .AddBlobContainer("photos");
+
+builder.AddProject<Projects.WebApplication1>("webapp")
+        .WithReference(photos)
+        .WaitFor(photos);
+
 builder.Build().Run();
--- PhotoGallery/WebApplication1/Program.cs
+++ PhotoGallery/WebApplication1/Program.cs
@@ -1,6 +1,24 @@
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Http.HttpResults;
+using WebApplication1.Components;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddAzureBlobContainerClient("photos");
+builder.Services.AddRazorComponents();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (BlobContainerClient client) =>
+{
+    var blobs = client.GetBlobsAsync();
+    var photos = new List<string>();
+    await foreach (var photo in blobs)
+    {
+        photos.Add(photo.Name);
+    }
+    return new RazorComponentResult<PhotoList>(new { Photos = photos });
+});
 
 app.Run();
+ 
