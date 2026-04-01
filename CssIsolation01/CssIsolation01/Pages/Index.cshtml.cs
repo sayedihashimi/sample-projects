01a6bb74f0fa283111b1d6640ead431363ce98fe
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2026-04-01 10:45:32 -0400

    adding css isolation sample

---
+++ CssIsolation01/CssIsolation01/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CssIsolation01.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
